c730def4eb096b8eb153a6dabbd389e48503538a
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2023-03-26 16:10:45 +1100

    bump version #2

--- dist/index.js
+++ dist/index.js
@@ -40,7 +40,7 @@ var http = /*#__PURE__*/Object.freeze({
 });
 
 /**
- * thena@0.0.6
+ * thena@0.0.7
  * A browser-safe, simple, lightweight, and fast utility library for JavaScript
  */
 const loop = (n, fn) => {
